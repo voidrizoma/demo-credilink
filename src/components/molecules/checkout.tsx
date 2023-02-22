c4202d1ca48dc9d772c71f15b6916998040d5319
francisco garrido <fg0611@gmail.com>
2023-02-22 12:53:03 -0300

    added sapic and more demo logic

--- src/components/molecules/checkout.tsx
+++ src/components/molecules/checkout.tsx
@@ -1,7 +1,8 @@
-import { component$, $ } from "@builder.io/qwik";
+import { component$, $, useStore } from "@builder.io/qwik";
 import { Credilink } from "~/models/credilink-model";
 import { issuerLogoFinder } from "~/helpers/issuer-methods";
 import { CheckoutModel } from "~/models/checkout-model";
+import { prepareMailData } from "~/helpers/mailDataGenerator";
 
 export interface IProps {
   credilink: Credilink;
@@ -10,16 +11,57 @@ export interface IProps {
 
 export default component$((props: IProps) => {
   // NAVIGATES TO /?loan=<ID>
+
+  const state = useStore({
+    currentRadio: "",
+  });
+
   const checkoutSubmit = $(async (loanId: string) => {
+
+    const expiresIn = new Date();
+    expiresIn.setHours(23, 59, 59, 0).toLocaleString();
+    const mailServiceUrl =
+      "https://paywithflux-services-notifier-staging.services.k8s.fluxqr.net/";
+
+    // const config = {
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     "Access-Control-Allow-Origin": mailServiceUrl,
+    //   },
+    // };
+
+    try {
+      await fetch(mailServiceUrl, {
+        method: "POST",
+        // mode: "no-cors",
+        headers: {
+          "Content-type": "application/json",
+          "Access-Control-Allow-Origin": mailServiceUrl,
+        },
+        body: prepareMailData(
+          props.credilink,
+          props.checkout,
+          expiresIn.toString(),
+          ""
+        ),
+      }).then(async (res) => {
+        if (res.status >= 200 && res.status < 300) {
+          const { data } = await res.json();
+          console.log("mailsent", JSON.stringify(data));
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
     window.location.href = `/?loan=${loanId}`;
   });
 
   return (
     <>
       <div class="flex w-[100%] place-content-center">
-        <div class="flex flex-col gap-8 rounded m-8 p-4">
+        <div class="flex flex-col gap-4 rounded m-8 p-4">
           <img
-            class="h-[10%]"
+            class="pb-3 h-[10%]"
             src={issuerLogoFinder(props.checkout.issuer)}
             alt=""
           />
@@ -29,25 +71,101 @@ export default component$((props: IProps) => {
             <div class="flex flex-col gap-3">
               <div>Con tu correo</div>
               <div>Vas a comprar en</div>
-              <div>Monto de tu compra</div>
             </div>
 
             {/* C2 */}
             <div class="flex flex-col gap-3 text-end">
               <div>{props.checkout.userData.email}</div>
               <div>{props.credilink.commerceName}</div>
-              <div
-                class={`text-[${props.checkout.issuer.color}] font-bold text-[21px]`}
-              >{`$${parseFloat(props.checkout.userData.amount)}.00`}</div>
             </div>
           </div>
-          <div class="flex place-content-center text-[20px] font-semibold">
+
+          {/* QUOTES */}
+          <div class="font-bold text-[26px] m-0 p-0">
+            Selecciona el plazo de tu crédito por
+          </div>
+          <div
+            class={`m-0 p-0 text-[${props.checkout.issuer.color}] font-bold text-[26px] text-blue-500`}
+          >{`$${parseFloat(props.checkout.userData.amount)}.00`}</div>
+
+          <div class="flex flex-col border-4 border-[#c2c2c2] rounded-md">
+            <div class="flex flex-row p-3">
+              <div class="flex place-content-center">
+                <input
+                  type="radio"
+                  class="scale-[1.8]"
+                  checked={state.currentRadio === "q1"}
+                  onClick$={() => {
+                    state.currentRadio = "q1";
+                  }}
+                />
+              </div>
+              <div class="px-2 text-[18px] font-bold text-blue-500">{`1 Quincena de $${
+                parseFloat(props.checkout.userData.amount) * 0.8 +
+                parseFloat(props.checkout.userData.amount) * 0.8 * (0.27 / 100)
+              }`}</div>
+            </div>
+            <div class="flex flex-row text-[14px]">
+              <div class="flex flex-auto p-2 border-t-4 border-r-4 border-b-0 border-l-0 border-[#c2c2c2] ">
+                Tasa de interés: 0.27%
+              </div>
+              <div class="flex flex-auto p-2 border-t-4 border-r-0 border-b-0 border-l-0 border-[#c2c2c2]">
+                {`Tasa de interés: $${
+                  parseFloat(props.checkout.userData.amount) *
+                  0.8 *
+                  (0.27 / 100)
+                }`}
+              </div>
+            </div>
+          </div>
+
+          <div class="flex flex-col border-4 border-[#c2c2c2] rounded-md">
+            <div class="flex flex-row p-3">
+              <div class="flex place-content-center">
+                <input
+                  type="radio"
+                  class="scale-[1.8]"
+                  checked={state.currentRadio === "q2"}
+                  onClick$={() => {
+                    state.currentRadio = "q2";
+                  }}
+                />
+              </div>
+              <div class="px-2 text-[18px] font-bold text-blue-500">{`2 Quincenas de $${
+                (parseFloat(props.checkout.userData.amount) * 0.8) / 2 +
+                ((parseFloat(props.checkout.userData.amount) * 0.8) / 2) *
+                  (0.27 / 100)
+              }`}</div>
+            </div>
+          </div>
+
+          <div class="flex flex-col border-4 border-[#c2c2c2] rounded-md">
+            <div class="flex flex-row p-3">
+              <div class="flex place-content-center">
+                <input type="radio"
+                  class="scale-[1.8]"
+                  checked={state.currentRadio === "q3"}
+                  onClick$={() => {
+                    state.currentRadio = "q3";
+                  }} />
+              </div>
+              <div class="px-2 text-[18px] font-bold text-blue-500">{`3 Quincenas de $${
+                (parseFloat(props.checkout.userData.amount) * 0.8) / 3 +
+                ((parseFloat(props.checkout.userData.amount) * 0.8) / 3) *
+                  (0.27 / 100)
+              }`}</div>
+            </div>
+          </div>
+
+          <div class="flex place-content-center text-[20px] font-semibold pt-3">
             <button
               class="text-white rounded-[20px] border-none h-[40px] w-full"
-              style={{ background: props.credilink.colorPrimary }}
+              style={{ background: state.currentRadio?.length ? props.credilink.colorPrimary : "#c2c2c2" }}
+              preventdefault:click
               onClick$={() => {
                 checkoutSubmit(props.checkout.issuer.id);
               }}
+              disabled={!state.currentRadio?.length}
             >
               Continuar
             </button>
--- src/helpers/mailDataGenerator.ts
+++ src/helpers/mailDataGenerator.ts
@@ -1,62 +1,44 @@
+import { CheckoutModel } from "~/models/checkout-model";
 import { Credilink } from "~/models/credilink-model";
 
 export const prepareMailData = (
   credilink: Credilink,
-  store: any,
+  checkout: CheckoutModel,
+  expiresIn: string,
   qr: string
 ) => {
-  const info = {
-    credilink: credilink.bg,
-    store,
-    qr
-    // template_name: regalink.template,
-    // template_content: [{}],
-    // message: {
-    //   subject: regalink.subject,
-    //   from_name: regalink.senderName,
-    //   from_email: regalink.sender,
-    //   to: [
-    //     {
-    //       email: store.customer.email,
-    //       type: "to",
-    //     },
-    //   ],
-    //   track_opens: true,
-    //   track_clicks: true,
-    //   merge_language: "handlebars",
-    //   global_merge_vars: [
-    //     {
-    //       name: "name",
-    //       content: name,
-    //     },
-    //     {
-    //       name: "body",
-    //       content: regalink.bodyMail,
-    //     },
-    //     {
-    //       name: "cupon",
-    //       content: encodeURIComponent(qr),
-    //     },
-    //     {
-    //       name: "logoCommerce",
-    //       content: regalink.logo,
-    //     },
-    //     {
-    //       name: "terms",
-    //       content: regalink.terms,
-    //     },
-    //     {
-    //       name: "amount",
-    //       content: regalink.amount / 100,
-    //     },
-    //     {
-    //       name: "expiration",
-    //       content: regalink.expiration,
-    //     },
-    //   ],
-    // },
-  };
-  return info;
+  qr;
+  const result = JSON.stringify({
+    template: "kueski",
+    subject: credilink.subject,
+    senderName: "Francisco",
+    sender: "regalink@fluxqr.com",
+    to: [
+      {
+        email: checkout.userData.email,
+      },
+    ],
+    globalMergeVars: [
+      {
+        name: "amount",
+        content: parseInt(checkout.userData.amount) / 100,
+      },
+      {
+        name: "cupon",
+        content: encodeURIComponent("tu crédito es éste"),
+      },
+      {
+        name: "logoCommerce",
+        content: credilink.logo,
+      },
+      {
+        name: "expiration",
+        content: expiresIn,
+      },
+    ],
+  });
+  console.log(result)
+  return result;
 };
 
 export const mailChimpMock = (templateData: any) => {
