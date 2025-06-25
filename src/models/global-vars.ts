ce28573c8997c2915df6d7ba2ec8068b2b7f0932
Karenina <karenina@Mac-mini-de-Karenina.local>
2025-06-25 14:37:04 -0600

    fix envs

--- src/models/global-vars.ts
+++ src/models/global-vars.ts
@@ -1,23 +1,22 @@
 export interface EnvVars {
-    apiUrl: string
-    refreshToken: string
-}
-
-// const apiExt= globalVars.apiUrl;
-// const apiLocal = import.meta.env.VITE_API || "https://flux-api-six.vercel.app/";
-const apiFlux = import.meta.env.VITE_API_FLUX;
-const fixedEmail = import.meta.env.VITE_FIXED_EMAIL;
-const refreshToken = import.meta.env.VITE_REFRESH_TOKEN;
-const urlZapier = 'https://hooks.zapier.com/hooks/catch/5474575/398esh3/';
-
-
-console.log(apiFlux)
-
-export const envVars = {
-    apiUrlFlux: apiFlux,
-    // tokenFlux,
+    apiUrl: string;
+    refreshToken: string;
+  }
+  
+  const apiFlux = import.meta.env.VITE_API_FLUX;
+  const fixedEmail = import.meta.env.VITE_FIXED_EMAIL;
+  const refreshToken = import.meta.env.VITE_REFRESH_TOKEN;
+  const urlZapier = 'https://hooks.zapier.com/hooks/catch/5474575/398esh3/';
+  
+  // Validación para evitar errores silenciosos en producción
+  if (!apiFlux) {
+    console.warn("⚠️ VITE_API_FLUX is not defined. Check your environment variables.");
+  }
+  
+  export const envVars = {
+    apiUrlFlux: apiFlux || "https://api.fluxqr.dev/v4", // fallback si estás en desarrollo
     fixedEmail,
     urlZapier,
     refreshToken
-}
-
+  };
+  
